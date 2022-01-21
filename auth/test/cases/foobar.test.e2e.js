f774176db88ce19a7ffa9bc9f9a5d4febce1aee1
Marco Pegoraro <marco.pegoraro@gmail.com>
2022-01-21 09:50:17 +0000

    Fix auth tests to run in GitPod

--- auth/test/cases/foobar.test.e2e.js
+++ auth/test/cases/foobar.test.e2e.js
@@ -2,6 +2,7 @@ describe("Foobar", () => {
   it("should work", async () => {
     // Use test globals:
     console.log(`FOOBAR = ${env.FOOBAR}`);
+    expect(env.FOOBAR).toBeDefined();
 
     // Read some App's configuration:
     const r1 = await getConfig("jwt.secret");
